aad131ff56faada8e2a9ae6484c29ddc8f1b0d13
pedro <pedro.h.a.moura.business@gmail.com>
2025-11-24 18:35:36 +0000

    ✨ | feat(024): create "shared" folder to reduce code repetition between resources that both client and server must use

---
+++ shared/config/events/player/actions.ts
@@ -0,0 +1,4 @@
+export const PLAYER_ACTIONS = {
+  JOINED: "player_joined",
+  MOVED: "player_moved",
+};
--- shared/types/room/state.ts
+++ shared/types/room/state.ts
@@ -1,5 +1,5 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
-import { Player } from "../player"
+import { Player } from "../player/schema";
 
 export class State extends Schema {
   @type({ map: Player }) players = new MapSchema<Player>();
