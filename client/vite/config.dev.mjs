192d2fc439f2db2448c708cacc97a90e67d56725
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2025-11-29 12:12:28 +0000

    :fire: | del(024): remove unecessary comment
    Co-authored-by: Ana Fernandes <anatfernandes.dev@gmail.com>

--- client/vite/config.dev.mjs
+++ client/vite/config.dev.mjs
@@ -24,7 +24,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '../src'),
-            'shared': path.resolve(__dirname, '../../shared'),  // ← Add this
+            'shared': path.resolve(__dirname, '../../shared'),
         }
     },
   };
