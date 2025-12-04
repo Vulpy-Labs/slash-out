33897825760f13c956b516130ed00dc512ef35de
pedro <pedro.h.a.moura.business@gmail.com>
2025-12-04 18:57:04 +0000

    🏷️ | type(040): add isGrounded properties in the player's schema

--- shared/types/player/schema.ts
+++ shared/types/player/schema.ts
@@ -3,4 +3,5 @@ import { Schema, type } from "@colyseus/schema";
 export class Player extends Schema {
   @type("number") x: number = 0;
   @type("number") y: number = 0;
+  @type("boolean") isGrounded: boolean = true;
 }
