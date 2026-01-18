a809c47ad63ffe392a730648e683a71c285de83f
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-18 20:32:59 +0000

    ✨ | feat(023): add default keymaps for player controls in ECS keymap system

---
+++ packages/client/src/game/utils/factories/ecs/components/keymap/defaultKeymap.ts
@@ -0,0 +1,25 @@
+import { KeymapComponent } from '@/ecs/components';
+
+const defaultKeymapP1 = (): KeymapComponent => ({
+  up: 'W',
+  down: 'S',
+  left: 'A',
+  right: 'D',
+  jump: 'K',
+  dash: 'L',
+  sword: 'J',
+  gun: 'I',
+});
+
+const defaultKeymapP2 = (): KeymapComponent => ({
+  up: 'UP',
+  down: 'DOWN',
+  left: 'LEFT',
+  right: 'RIGHT',
+  jump: 'NUMPAD_ONE',
+  dash: 'NUMPAD_TWO',
+  sword: 'NUMPAD_THREE',
+  gun: 'NUMPAD_FOUR',
+});
+
+export { defaultKeymapP1, defaultKeymapP2 };
