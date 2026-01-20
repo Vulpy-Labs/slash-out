3bb41dfb4ea166b0665def9d262eff60fa456615
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-20 20:59:52 +0000

    ✨ | feat(024): integrate MovementFactory into MatchScene for player movement configuration

--- packages/client/src/game/scenes/game/match/MatchScene.ts
+++ packages/client/src/game/scenes/game/match/MatchScene.ts
@@ -2,23 +2,35 @@ import { MatchConfig } from '@/ecs/components';
 import { InputSystem, KeymapSystem } from '@/ecs/systems';
 import { GlobalEntityMap } from './type.i';
 import { defaultInput, defaultKeymap } from '@/utils/factories/ecs/components';
+import { MovementFactory } from '@/utils/factories/ecs/components/movement';
 
 export class MatchScene extends Phaser.Scene {
   private matchConfig?: MatchConfig;
+
+  private movementFactory: MovementFactory;
 
   private entities: GlobalEntityMap = new Map();
 
   private keymapSystem: KeymapSystem;
   private inputSystem: InputSystem;
 
+  // Handle Super
   constructor() {
     super('MatchScene');
   }
 
+  // Initialize Classes
   init(data: MatchConfig) {
     this.matchConfig = data;
+
+    this.initFactories();
   }
 
+  initFactories() {
+    this.movementFactory = new MovementFactory();
+  }
+
+  // Create Elements
   create() {
     this.createECS();
     this.createKeyboardInputs();
@@ -38,7 +50,10 @@ export class MatchScene extends Phaser.Scene {
       entityId: 'player_01',
       keymap: defaultKeymap({ player: '01' }),
       input: defaultInput(),
+      movement: this.movementFactory.getDefaultMovement(),
     };
+
+    console.log('fakeEntity', fakeEntity);
 
     this.entities.set(fakeEntity.entityId, fakeEntity);
   }
