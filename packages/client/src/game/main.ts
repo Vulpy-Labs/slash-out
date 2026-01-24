d8904983cb8d96e0d449f9d02a0bb45a7d00a22d
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-24 14:07:20 +0000

    🔧 | config(025): update game configuration to use SCENE constants for width, height, and physics settings

--- packages/client/src/game/main.ts
+++ packages/client/src/game/main.ts
@@ -2,15 +2,45 @@ import { AUTO, Game } from 'phaser';
 import { Boot, Preloader } from './scenes/core';
 import { MainMenu } from './scenes/ui/menu';
 import { MatchScene } from './scenes/game';
+import { PHYSICS, SCENE } from './config/constants';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
+  width: SCENE.WIDTH,
+  height: SCENE.HEIGHT,
   parent: 'game-container',
   backgroundColor: '#028af8',
+  physics: {
+    default: 'matter',
+    matter: {
+      gravity: { x: 0, y: PHYSICS.GRAVITY },
+      debug: true,
+      setBounds: {
+        x: 0,
+        y: 0,
+        width: SCENE.WIDTH,
+        height: SCENE.HEIGHT,
+        thickness: 64,
+        left: true,
+        right: true,
+        top: true,
+        bottom: true,
+      },
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  render: {
+    pixelArt: true,
+    antialias: false,
+  },
+  input: {
+    keyboard: true,
+  },
   scene: [Boot, Preloader, MainMenu, MatchScene],
 };
 
