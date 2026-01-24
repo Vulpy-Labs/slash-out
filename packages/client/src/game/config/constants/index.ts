892ff4559b782eb206b225d303be2e6d3d774f00
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-24 14:07:04 +0000

    ✨ | feat(025): add physics constants and update exports in constants index

--- packages/client/src/game/config/constants/index.ts
+++ packages/client/src/game/config/constants/index.ts
@@ -1,2 +1,4 @@
 export * from './possible-actions';
 export * from './movement';
+export * from './scene';
+export * from './physics';
---
+++ packages/client/src/game/config/constants/physics/physics-constant.ts
@@ -0,0 +1,5 @@
+const PHYSICS = {
+  GRAVITY: 1,
+};
+
+export { PHYSICS };
