5cf5c6a970b81d6461b3918d33df886e4a1a1614
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-28 15:27:59 +0000

    🏷️ | type(030): update PlayerFactoryProp to use characterName and add loadCharacterSpritesProp type

--- packages/client/src/game/utils/factories/ecs/entities/player/types.p.ts
+++ packages/client/src/game/utils/factories/ecs/entities/player/types.p.ts
@@ -1,11 +1,13 @@
 import { GlobalEntityMap } from '@/scenes/game';
 
+type CharacterNames = 'hayao' | 'hiroshige' | 'habuki' | 'kurosawa' | 'otomo';
+
 type PlayerFactoryProp = {
   scene: Phaser.Scene;
   entities: GlobalEntityMap;
   x: number;
   y: number;
-  defaultSprite: string;
+  characterName: CharacterNames;
   keymapFor: '01' | '02';
 };
 
@@ -13,4 +15,10 @@ type GetPlayerIdProp = {
   entities: GlobalEntityMap;
 };
 
-export type { PlayerFactoryProp, GetPlayerIdProp };
+type loadCharacterSpritesProp = {
+  name: CharacterNames;
+  scene: Phaser.Scene;
+  version: 'v1' | 'v2' | 'v3';
+};
+
+export type { PlayerFactoryProp, GetPlayerIdProp, loadCharacterSpritesProp };
