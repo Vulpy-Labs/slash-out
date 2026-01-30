ffd644193c537213d62266b239bdf78c98556185
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-30 15:39:16 +0000

    ✨ | feat(043): implement PlayerBuilder class for player entity creation and sprite loading

---
+++ packages/client/src/game/utils/builders/ecs/entities/player/PlayerBuilder.ts
@@ -0,0 +1,117 @@
+import { PlayerEntity } from '@/ecs/entities/player';
+import { CreatePlayerSpriteProp, MountPlayerEntityProp, PlayerBuilderProp } from './types.p';
+import { defaultInput, defaultKeymap, defaultMovement } from '@/utils/factories/ecs/components';
+import { CHARACTERS_SPRITES_MODEL, DEPTH, PLAYER_DIMENSIONS } from '@/config/constants';
+import { MatchConfig } from '@/ecs/components';
+import { GlobalEntityMap } from '@/scenes/game';
+
+class PlayerBuilder {
+  private scene: Phaser.Scene;
+  private matchConfig: MatchConfig;
+  private entities: GlobalEntityMap;
+
+  private baseCharacterSpritesPath = 'assets/sprites/characters';
+
+  // ! Remove after the implementation of the respawn system
+  private tempSpawnPoints = {
+    x: 100,
+    y: 100,
+  };
+
+  constructor({ scene, matchConfig, entities }: PlayerBuilderProp) {
+    this.scene = scene;
+    this.matchConfig = matchConfig;
+    this.entities = entities;
+  }
+
+  load() {
+    this.loadCharacterSprites();
+  }
+
+  build() {
+    this.createPlayers();
+  }
+
+  private loadCharacterSprites() {
+    this.matchConfig.players.characters.forEach(character => {
+      const characterSprites = CHARACTERS_SPRITES_MODEL[character.name];
+
+      if (!characterSprites) {
+        throw new Error(`Sprite model not found for character: ${character.name}`);
+      }
+
+      characterSprites.forEach(({ isSpritesheet, spriteName }) => {
+        const key = `${character.name}_${spriteName}`;
+        const url = `${this.baseCharacterSpritesPath}/${character.name}/${character.skin}/${spriteName}.png`;
+
+        if (isSpritesheet) {
+          return this.scene.load.spritesheet(key, url, {
+            frameWidth: PLAYER_DIMENSIONS.WIDTH,
+            frameHeight: PLAYER_DIMENSIONS.HEIGHT,
+          });
+        }
+
+        return this.scene.load.image({
+          key,
+          url,
+        });
+      });
+    });
+  }
+
+  private getPlayerId() {
+    let playersQuant = 0;
+
+    this.entities.forEach(({ entityId }) =>
+      entityId.startsWith('player_') ? playersQuant++ : null
+    );
+
+    return `player_0${playersQuant + 1}`;
+  }
+
+  private createPlayers() {
+    this.matchConfig.players.characters.forEach(character => {
+      const playerId = this.getPlayerId();
+      const playerSprite = this.createPlayerSprite({ character, options: { friction: 0 } });
+      const playerEntity: PlayerEntity = this.mountPlayerEntity({
+        entityId: playerId,
+        character,
+        sprite: playerSprite,
+      });
+
+      this.entities.set(playerEntity.entityId, playerEntity);
+    });
+  }
+
+  private createPlayerSprite({ character, frame, options }: CreatePlayerSpriteProp) {
+    const intialArt = `${character.name}_spr_idle`;
+    const sprite = this.scene.matter.add.sprite(
+      this.tempSpawnPoints.x,
+      this.tempSpawnPoints.y,
+      intialArt,
+      frame,
+      options
+    );
+
+    sprite.setFixedRotation();
+    sprite.setDepth(DEPTH.ENTITIES);
+
+    return sprite;
+  }
+
+  private mountPlayerEntity({ entityId, character, sprite }: MountPlayerEntityProp) {
+    return {
+      entityId,
+      character: {
+        name: character.name,
+        skin: character.skin,
+        sprite: sprite,
+      },
+      keymap: defaultKeymap({ player: character.playerRef || '01' }),
+      input: defaultInput(),
+      movement: defaultMovement({ entityType: 'player' }),
+    };
+  }
+}
+
+export { PlayerBuilder };
