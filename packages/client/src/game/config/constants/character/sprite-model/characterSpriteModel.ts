8f3205f747b2c9e7bdc1aa3ee8b01819609cf499
pedro <pedro.h.a.moura.business@gmail.com>
2026-01-30 15:38:21 +0000

    🔧 | config(043): add character sprite model constants and update exports

---
+++ packages/client/src/game/config/constants/character/sprite-model/characterSpriteModel.ts
@@ -0,0 +1,314 @@
+const CHARACTERS_SPRITES_MODEL = {
+  hayao: [
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_dead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_deadnohead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_running',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_jump',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_head',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_idle',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_slice',
+    },
+  ],
+  hiroshige: [
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_dead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_deadnohead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_running',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_jump',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_head',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_idle',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_slice',
+    },
+  ],
+  habuki: [
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_dead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_deadnohead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_running',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_jump',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_head',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_idle',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_slice',
+    },
+  ],
+  kurosawa: [
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_dead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_deadnohead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_running',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_jump',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_head',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_idle',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_slice',
+    },
+  ],
+  otomo: [
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_dead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_deadnohead',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_running',
+    },
+    {
+      isSpritesheet: true,
+      spriteName: 'spr_jump',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_head',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_idle',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_longattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_lookup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattack',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackdown',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_shortattackup',
+    },
+    {
+      isSpritesheet: false,
+      spriteName: 'spr_slice',
+    },
+  ],
+};
+
+export { CHARACTERS_SPRITES_MODEL };
