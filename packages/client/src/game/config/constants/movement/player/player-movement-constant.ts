d0ac376e3c59e0d720d11d7e82340825d87b4bc0
anatfernandes <analuizateixeirafernandes184@gmail.com>
2026-01-31 15:51:00 -0300

    ♻️ | refac(045):  change constants to use 'as const' syntax

--- packages/client/src/game/config/constants/movement/player/player-movement-constant.ts
+++ packages/client/src/game/config/constants/movement/player/player-movement-constant.ts
@@ -1,4 +1,4 @@
-export const PLAYER_MOVEMENT_CONSTANT = {
+const PLAYER_MOVEMENT_CONSTANT = {
   AIR: {
     FRICTION: 0.05,
     SPEED: 5,
@@ -7,4 +7,6 @@ export const PLAYER_MOVEMENT_CONSTANT = {
     FRICTION: 0.21,
     SPEED: 5,
   },
-};
+} as const;
+
+export { PLAYER_MOVEMENT_CONSTANT };
