016fdaeb08893cc7556fefb390b182a460aa675e
pedro <pedro.h.a.moura.business@gmail.com>
2026-02-09 18:31:14 +0000

    🚨 | lint(048): break lint formatation to test build-client lint validation

--- packages/client/src/game/ecs/components/character/CharacterComponent.ts
+++ packages/client/src/game/ecs/components/character/CharacterComponent.ts
@@ -2,7 +2,11 @@ import { CHARACTER_NAMES, CHARACTER_SKINS } from '@/config/constants';
 
 interface CharacterComponent {
   skin: (typeof CHARACTER_SKINS)[number];
+
   name: (typeof CHARACTER_NAMES)[number];
+
+
+
   sprite?: Phaser.Physics.Matter.Sprite;
 }
 
