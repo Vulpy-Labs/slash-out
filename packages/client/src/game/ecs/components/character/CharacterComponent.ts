323554bd1d799dcaa6c31f21a51ff933d34fb302
pedro <pedro.h.a.moura.business@gmail.com>
2026-02-09 18:37:05 +0000

    🚨 | lint(048): remove unnecessary blank lines in CharacterComponent interface

--- packages/client/src/game/ecs/components/character/CharacterComponent.ts
+++ packages/client/src/game/ecs/components/character/CharacterComponent.ts
@@ -2,11 +2,7 @@ import { CHARACTER_NAMES, CHARACTER_SKINS } from '@/config/constants';
 
 interface CharacterComponent {
   skin: (typeof CHARACTER_SKINS)[number];
-
   name: (typeof CHARACTER_NAMES)[number];
-
-
-
   sprite?: Phaser.Physics.Matter.Sprite;
 }
 
