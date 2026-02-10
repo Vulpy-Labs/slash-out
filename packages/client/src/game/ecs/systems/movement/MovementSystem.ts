c843c4043ccf77f249fc3a82c51013bee20240d8
anatfernandes <analuizateixeirafernandes184@gmail.com>
2026-02-10 17:38:15 -0300

    ✨ | feat(031): temporarily remove animation flipX from movement system

--- packages/client/src/game/ecs/systems/movement/MovementSystem.ts
+++ packages/client/src/game/ecs/systems/movement/MovementSystem.ts
@@ -8,7 +8,7 @@ class MovementSystem {
   }
 
   update({ entities }: MovementSystemUpdateProp) {
-    entities.forEach(({ input, movement, animation, character }) => {
+    entities.forEach(({ input, movement, character }) => {
       if (!input || !movement || !character?.sprite?.body) return;
 
       const wantsToMoveHorizontally = input.left || input.right;
@@ -20,18 +20,16 @@ class MovementSystem {
       if (wantsToMoveHorizontally) {
         if (input.left) {
           vx = -movement.ground.speed;
+          character.sprite.setFlipX(true);
         }
         if (input.right) {
           vx = movement.ground.speed;
+          character.sprite.setFlipX(false);
         }
       }
 
       if (wantsToJump) {
         vy = -movement.air.speed;
-      }
-
-      if (animation) {
-        animation.flipX = input.left || (!input.right && animation.flipX);
       }
 
       this.scene.matter.body.setVelocity(character.sprite.body as MatterJS.BodyType, {
