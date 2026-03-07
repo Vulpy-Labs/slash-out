214299a1b430caabedd1cee511b4bc02d45e7e03
anatfernandes <analuizateixeirafernandes184@gmail.com>
2026-03-07 15:31:23 -0300

    ♻️ | refac(061): update movement factory to new pattern

--- packages/client/src/game/utils/factories/ecs/components/movement/defaultMovement.ts
+++ packages/client/src/game/utils/factories/ecs/components/movement/defaultMovement.ts
@@ -1,20 +1,29 @@
-import { PLAYER_MOVEMENT } from '@/config/constants';
+import { MOVEMENT_MAPPING } from '@/config/constants';
 import { MovementComponent } from '@/ecs/components';
 import { DefaultMovementProp } from './type.p';
 
 function defaultMovement({ entityType }: DefaultMovementProp): MovementComponent {
-  const MOVEMENT_CONSTANT = {
-    player: PLAYER_MOVEMENT,
-  };
+  const config = MOVEMENT_MAPPING[entityType];
+  const entityMovement = config ?? MOVEMENT_MAPPING.fallback;
+
+  if (!config) {
+    console.warn(
+      `No config was found to build the default movement for the entity: ${entityType}. Using fallback...`
+    );
+  }
 
   return {
+    intent: {
+      moveX: 0,
+      moveY: 0,
+    },
     air: {
-      speed: MOVEMENT_CONSTANT[entityType].AIR.SPEED,
-      friction: MOVEMENT_CONSTANT[entityType].AIR.FRICTION,
+      speed: entityMovement.AIR.SPEED,
+      friction: entityMovement.AIR.FRICTION,
     },
     ground: {
-      speed: MOVEMENT_CONSTANT[entityType].GROUND.SPEED,
-      friction: MOVEMENT_CONSTANT[entityType].GROUND.FRICTION,
+      speed: entityMovement.GROUND.SPEED,
+      friction: entityMovement.GROUND.FRICTION,
     },
   };
 }
