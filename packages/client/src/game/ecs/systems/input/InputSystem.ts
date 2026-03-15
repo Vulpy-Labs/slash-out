fb4376806c234fead826006cbd968e1ee414b207
anatfernandes <analuizateixeirafernandes184@gmail.com>
2026-03-15 18:45:04 -0300

    ♻️ | refac(041): remove unnecessary code

--- packages/client/src/game/ecs/systems/input/InputSystem.ts
+++ packages/client/src/game/ecs/systems/input/InputSystem.ts
@@ -1,22 +1,8 @@
 import { CONFLICTING_ACTIONS, PossibleActions } from '@/config/constants';
-import { InputSystemProps, InputSystemUpdateProps } from './types.p';
+import { InputSystemUpdateProps } from './types.p';
 
 class InputSystem {
-  private scene: Phaser.Scene;
-
-  constructor({ scene }: InputSystemProps) {
-    if (!scene) {
-      throw new Error('scene parameter is missing or invalid');
-    }
-
-    this.scene = scene;
-  }
-
   update({ entities }: InputSystemUpdateProps) {
-    if (!(entities instanceof Map)) {
-      throw new Error(`Entities is not a Map: ${typeof entities}`);
-    }
-
     entities.forEach(({ keymap, input }) => {
       if (!keymap || !input) return;
 
