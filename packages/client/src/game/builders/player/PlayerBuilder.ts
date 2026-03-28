dc6b03a9f368679c04bb0e1e04d3d27c2ec31af6
anatfernandes <analuizateixeirafernandes184@gmail.com>
2026-03-28 09:58:16 -0300

    ♻️ | refac(073): change the PlayerBuilder logic to allow individual player construction

--- packages/client/src/game/builders/player/PlayerBuilder.ts
+++ packages/client/src/game/builders/player/PlayerBuilder.ts
@@ -9,17 +9,16 @@ import {
 } from '@/utils/factories/ecs/components';
 
 import { PlayerEntity } from '@/ecs/entities';
-import { MatchConfigPlayers } from '@/ecs/components';
 import {
   PlayerBuilderProp,
   MountPlayerEntityProp,
   CreatePlayerSpriteProp,
   OnEntityCreatedCallback,
+  PlayerBuilderPayloadProp,
 } from './types.p';
 
 class PlayerBuilder {
   private readonly scene: Phaser.Scene;
-  private readonly playersConfig: MatchConfigPlayers;
   private readonly onEntityCreated: OnEntityCreatedCallback;
 
   private readonly baseCharacterSpritesPath = 'assets/sprites/characters';
@@ -30,52 +29,47 @@ class PlayerBuilder {
     y: 100,
   };
 
-  constructor({ scene, playersConfig, onEntityCreated }: PlayerBuilderProp) {
+  constructor({ scene, onEntityCreated }: PlayerBuilderProp) {
     this.scene = scene;
-    this.playersConfig = playersConfig;
     this.onEntityCreated = onEntityCreated;
   }
 
-  load() {
-    this.loadCharacterSprites();
+  load({ character }: PlayerBuilderPayloadProp) {
+    this.loadCharacterSprites({ character });
   }
 
-  build() {
-    this.createPlayers();
+  build({ character }: PlayerBuilderPayloadProp) {
+    this.createPlayer({ character });
   }
 
-  private loadCharacterSprites() {
-    this.playersConfig.characters.forEach(character => {
-      const characterSprites = CHARACTERS_SPRITES_MODEL[character.name];
+  private loadCharacterSprites({ character }: PlayerBuilderPayloadProp) {
+    const characterSprites = CHARACTERS_SPRITES_MODEL[character.name];
 
-      if (!characterSprites) {
-        throw new Error(`Sprite model not found for character: ${character.name}`);
-      }
+    if (!characterSprites) {
+      throw new Error(`Sprite model not found for character: ${character.name}`);
+    }
 
-      characterSprites.forEach(({ spriteName }) => {
-        const key = `${character.name}_${spriteName}_${character.skin}`;
-        const url = `${this.baseCharacterSpritesPath}/${character.name}/${character.skin}/${spriteName}.png`;
+    characterSprites.forEach(({ spriteName }) => {
+      const key = `${character.name}_${spriteName}_${character.skin}`;
+      const url = `${this.baseCharacterSpritesPath}/${character.name}/${character.skin}/${spriteName}.png`;
 
-        if (this.scene.textures.exists(key)) return;
+      if (this.scene.textures.exists(key)) return;
 
-        return this.scene.load.spritesheet(key, url, {
-          frameWidth: PLAYER_DIMENSIONS.WIDTH,
-          frameHeight: PLAYER_DIMENSIONS.HEIGHT,
-        });
+      return this.scene.load.spritesheet(key, url, {
+        frameWidth: PLAYER_DIMENSIONS.WIDTH,
+        frameHeight: PLAYER_DIMENSIONS.HEIGHT,
       });
     });
   }
 
-  private createPlayers() {
-    this.playersConfig.characters.forEach(character => {
-      const playerSprite = this.createPlayerSprite({ character, options: { friction: 0 } });
-      const playerEntity = this.mountPlayerEntity({
-        character,
-        sprite: playerSprite,
-      });
-
-      this.onEntityCreated(playerEntity);
+  private createPlayer({ character }: CreatePlayerSpriteProp) {
+    const playerSprite = this.createPlayerSprite({ character, options: { friction: 0 } });
+    const playerEntity = this.mountPlayerEntity({
+      character,
+      sprite: playerSprite,
     });
+
+    this.onEntityCreated(playerEntity);
   }
 
   private createPlayerSprite({ character, frame, options }: CreatePlayerSpriteProp) {
@@ -103,10 +97,10 @@ class PlayerBuilder {
         skin: character.skin,
       },
       input: defaultInput(),
+      state: defaultState(),
       animation: defaultPlayerAnimation({ character }),
       movement: defaultMovement({ entityType: 'player' }),
       keymap: defaultKeymap({ player: character.playerRef }),
-      state: defaultState(),
     };
   }
 }
