e6d62b0f455429864c40830c7a830c0ce7299daf
pedro <pedro.h.a.moura.business@gmail.com>
2026-04-19 10:23:49 +0100

    ♻️ | refac(028): Simplify EntityTypes definition by removing DeepValues type

--- packages/client/src/game/config/constants/entity-types/type.t.ts
+++ packages/client/src/game/config/constants/entity-types/type.t.ts
@@ -1,7 +1,5 @@
 import { ENTITY_TYPES } from './entityTypes';
 
-type DeepValues<T> = T extends string ? T : T extends object ? DeepValues<T[keyof T]> : never;
-
-type EntityTypes = DeepValues<typeof ENTITY_TYPES>;
+type EntityTypes = (typeof ENTITY_TYPES)[keyof typeof ENTITY_TYPES];
 
 export type { EntityTypes };
