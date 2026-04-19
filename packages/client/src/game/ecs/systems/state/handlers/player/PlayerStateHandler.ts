685007b4d43c61afef7154fcee5b8d2f868e2841
pedro <pedro.h.a.moura.business@gmail.com>
2026-04-19 13:05:17 +0100

    ✨ | feat(079): implement PlayerStateHandler for managing player states

---
+++ packages/client/src/game/ecs/systems/state/handlers/player/PlayerStateHandler.ts
@@ -0,0 +1,37 @@
+// PR - 5
+import { MOBILITY } from '@/config/constants';
+import { GlobalEntity } from '@/ecs/entities';
+import { getMobility } from '@/utils/physics';
+import { GroundedHandler, AirborneHandler } from './helpers';
+
+import { IEntityStateHandler } from '../types.i';
+import { PlayerStateHandlerUpdateProp, ValidPlayerEntity } from './types.p';
+
+class PlayerStateHandler implements IEntityStateHandler {
+  private groundedHandler: GroundedHandler;
+  private airborneHandler: AirborneHandler;
+
+  constructor() {
+    this.groundedHandler = new GroundedHandler();
+    this.airborneHandler = new AirborneHandler();
+  }
+
+  update({ entity }: PlayerStateHandlerUpdateProp): void {
+    if (!this.isValidPlayer(entity)) return;
+
+    const { state, input, sprite } = entity;
+    const mobility = getMobility(sprite);
+
+    if (mobility === MOBILITY.GROUNDED) {
+      this.groundedHandler.resolve({ state, input });
+    } else if (mobility === MOBILITY.AIRBORNE) {
+      this.airborneHandler.resolve({ state, input });
+    }
+  }
+
+  private isValidPlayer(entity: GlobalEntity): entity is ValidPlayerEntity {
+    return !!entity.input && !!entity.state && !!entity.sprite;
+  }
+}
+
+export { PlayerStateHandler };
