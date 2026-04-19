96aad811304e1a913865de18829cc397402c14e0
pedro <pedro.h.a.moura.business@gmail.com>
2026-04-19 12:59:36 +0100

    ♻️ | refac(079): simplify StateComponent and StateSystem by removing unused handlers and properties

--- packages/client/src/game/ecs/systems/state/StateSystem.ts
+++ packages/client/src/game/ecs/systems/state/StateSystem.ts
@@ -1,55 +1,23 @@
-import { ALIVE_STATE, MOBILITY, PLAYER_MOVEMENT } from '@/config/constants';
-import {
-  StateSystemUpdateProp,
-  StateSystemIsPlayerAliveProp,
-  StateSystemUpdatePostureProp,
-} from './types.p';
-import { DeadHandler, GroundedHandler, AirborneHandler } from './handlers';
+// PR - 5
+import { StateSystemUpdateProp } from './types.p';
+import { IEntityStateHandler, PlayerStateHandler } from './handlers';
+import { ENTITY_TYPES, EntityTypes } from '@/config/constants';
 
 class StateSystem {
-  private deadHandler: DeadHandler;
-  private groundedHandler: GroundedHandler;
-  private airborneHandler: AirborneHandler;
+  private handlers: Map<EntityTypes, IEntityStateHandler>;
 
   constructor() {
-    this.deadHandler = new DeadHandler();
-    this.groundedHandler = new GroundedHandler();
-    this.airborneHandler = new AirborneHandler();
+    this.handlers = new Map<EntityTypes, IEntityStateHandler>([
+      [ENTITY_TYPES.PLAYER, new PlayerStateHandler()],
+    ]);
   }
 
-  update({ entities }: StateSystemUpdateProp) {
-    entities.forEach(({ state, input, sprite }) => {
-      if (!state || !input || !sprite) return;
+  update({ entities }: StateSystemUpdateProp): void {
+    entities.forEach(entity => {
+      if (!entity.state) return;
 
-      if (!this.isPlayerAlive({ state })) {
-        this.deadHandler.resolve({ state });
-        return;
-      }
-
-      this.updatePosture({ state, sprite });
-
-      if (state.mobility === MOBILITY.GROUNDED) {
-        this.groundedHandler.resolve({ state, input });
-      } else if (state.mobility === MOBILITY.AIRBORNE) {
-        this.airborneHandler.resolve({ state, input });
-      }
+      this.handlers.get(entity.entityType)?.update({ entity });
     });
-  }
-
-  private isPlayerAlive({ state }: StateSystemIsPlayerAliveProp) {
-    return state.aliveState === ALIVE_STATE.ALIVE;
-  }
-
-  private updatePosture({ state, sprite }: StateSystemUpdatePostureProp): void {
-    const body = sprite.body as MatterJS.BodyType;
-    const vy = body.velocity.y;
-    const isGrounded = Math.abs(vy) < PLAYER_MOVEMENT.GROUND.GROUNDED_VELOCITY_THRESHOLD;
-
-    if (isGrounded) {
-      state.mobility = MOBILITY.GROUNDED;
-    } else {
-      state.mobility = MOBILITY.AIRBORNE;
-    }
   }
 }
 
--- packages/client/src/game/utils/factories/ecs/components/state/defaultState.ts
+++ packages/client/src/game/utils/factories/ecs/components/state/defaultState.ts
@@ -1,12 +1,9 @@
-import { INPUT_ACTION, ALIVE_STATE, CHARACTER_STATE, MOBILITY } from '@/config/constants';
+import { CHARACTER_STATE } from '@/config/constants';
 import { StateComponent } from '@/ecs/components';
 
 function defaultState(): StateComponent {
   return {
-    characterState: CHARACTER_STATE.IDLE,
-    mobility: MOBILITY.GROUNDED,
-    inputAction: INPUT_ACTION.NONE,
-    aliveState: ALIVE_STATE.ALIVE,
+    current: CHARACTER_STATE.IDLE,
   };
 }
 
