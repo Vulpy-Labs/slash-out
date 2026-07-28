9eb374376e854ab2e302e807f4358516eb97e400
Pedro <pedro.h.a.moura.business@gmail.com>
2026-07-28 18:17:43 -0300

    ✨ | feat(108): implement build method in gun weapon handler

--- packages/client/src/game/builders/weapon/handlers/gun/GunWeaponHandler.ts
+++ packages/client/src/game/builders/weapon/handlers/gun/GunWeaponHandler.ts
@@ -1,4 +1,6 @@
+import { BULLET, DEPTH, ENTITY_TYPES, GUN_STATE } from '@/config/constants';
 import { GlobalEntity } from '@/ecs/entities';
+import { defaultInput, defaultKeymap } from '@/utils/factories/ecs/components';
 import { IWeaponHandler } from '../types.i';
 
 export class GunWeaponHandler implements IWeaponHandler {
@@ -12,7 +14,38 @@ export class GunWeaponHandler implements IWeaponHandler {
     }
   }
 
-  build(): GlobalEntity {
-    throw new Error('Method not implemented.');
+  build({
+    scene,
+    ownerEntityId,
+  }: {
+    scene: Phaser.Scene;
+    x: number;
+    y: number;
+    ownerEntityId: string;
+  }): GlobalEntity {
+    const sprite = scene.matter.add.sprite(100, 100, 'spr_bullet_0');
+
+    sprite.setDisplaySize(BULLET.CONFIG.WIDTH, BULLET.CONFIG.HEIGHT);
+    sprite.setOrigin(BULLET.CONFIG.ORIGIN_X, BULLET.CONFIG.ORIGIN_Y);
+    sprite.setBody({
+      type: 'rectangle',
+      width: BULLET.CONFIG.WIDTH,
+      height: BULLET.CONFIG.HEIGHT,
+    });
+
+    sprite.setSensor(true);
+    sprite.setIgnoreGravity(true);
+    sprite.setDepth(DEPTH.ENTITIES);
+
+    const playerRef = ownerEntityId.includes('02') ? '02' : '01';
+
+    return {
+      entityId: `gun_${ownerEntityId}`,
+      entityType: ENTITY_TYPES.GUN,
+      sprite,
+      state: { current: GUN_STATE.IDLE },
+      input: defaultInput(),
+      keymap: defaultKeymap({ player: playerRef }),
+    };
   }
 }
