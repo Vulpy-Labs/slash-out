de6f57706030f6e406f2afd5e2eba116d17f5b42
Pedro <pedro.h.a.moura.business@gmail.com>
2026-07-29 17:52:25 -0300

    ✨ | feat(109): create gun state handler

---
+++ packages/client/src/game/ecs/systems/state/handlers/gun/GunStateHandler.ts
@@ -0,0 +1,47 @@
+import { ENTITY_TYPES, GUN_STATE } from '@/config/constants';
+import { InputComponent, StateComponent } from '@/ecs/components';
+import { GlobalEntity } from '@/ecs/entities';
+import { IEntityStateHandler } from '../types.i';
+import { GunStateHandlerUpdateProp, ValidGunEntity } from './types.p';
+
+class GunStateHandler implements IEntityStateHandler {
+  update({ entity }: GunStateHandlerUpdateProp): void {
+    if (!this.isValidGun(entity)) return;
+
+    const { state, input } = entity;
+
+    this.updateAttackLockState({ state, input });
+    this.resolveGunState({ state, input });
+  }
+
+  private updateAttackLockState({
+    state,
+    input,
+  }: {
+    state: StateComponent;
+    input: InputComponent;
+  }): void {
+    if (!input.gun) {
+      state.isAttackSpamming = false;
+    }
+  }
+
+  private resolveGunState({
+    state,
+    input,
+  }: {
+    state: StateComponent;
+    input: InputComponent;
+  }): void {
+    if (input.gun && !state.isAttackSpamming) {
+      state.current = GUN_STATE.FIRING;
+      state.isAttackSpamming = true;
+    }
+  }
+
+  private isValidGun(entity: GlobalEntity): entity is ValidGunEntity {
+    return !!entity.input && !!entity.state && entity.entityType === ENTITY_TYPES.GUN;
+  }
+}
+
+export { GunStateHandler };
--- packages/client/src/game/ecs/systems/state/handlers/index.ts
+++ packages/client/src/game/ecs/systems/state/handlers/index.ts
@@ -1,3 +1,4 @@
 export * from './player';
 export * from './sword';
+export * from './gun';
 export * from './types.i';
